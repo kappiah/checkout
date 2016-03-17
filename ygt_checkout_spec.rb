9179db09ee503bbb7a2745466bc6487f22ecd724
Kwasi Appiah <kwasi@appiah.id.au>
2016-03-17 14:23:20 +1100

    Adding items to the cart

--- ygt_checkout_spec.rb
+++ ygt_checkout_spec.rb
@@ -1,6 +1,15 @@
 require 'rspec'
 
 class Checkout
+  attr_reader :items
+
+  def initialize
+    @items = []
+  end
+
+  def scan(item)
+    @items << item
+  end
 
   def total
     0
@@ -12,5 +21,13 @@ describe "Checkout" do
   it "Show a zero total when empty" do
     checkout = Checkout.new
     expect(checkout.total).to eq 0
+  end
+
+  it "scans items into the cart" do
+    checkout = Checkout.new
+    item = double
+    checkout.scan(item)
+
+    expect(checkout.items.length).to eq 1
   end
 end
